52a16ddca827e3fdeeb22c5cc1d3a13a7cd3b856
volodymyr.havryliuk <volodymyr.havryliuk@demicon.de>
2020-03-15 19:22:25 +0200

    Update sample code

--- SourceCodeForAnalysis.cs
+++ SourceCodeForAnalysis.cs
@@ -67,8 +67,6 @@ namespace TopLevel
 
         public interface ICustomer
         {
-            IEnumerable<IOrder> PreviousOrders { get; }
-            DateTime? LastOrder { get; }
             string Name { get; }
             IDictionary<DateTime, string> Reminders { get; }
 
@@ -97,16 +95,29 @@ namespace TopLevel
 
         class Foo
         {
-			        public interface IOrder
-        {
-            DateTime Purchased { get; }
-            decimal Cost { get; }
-            string newMethod()
-            {
-                return "string";
-            }
-        }
 
+
+        public interface ICustomer
+        {
+            IEnumerable<IOrder> PreviousOrders { get; }
+
+            DateTime DateJoined { get; }
+            DateTime? LastOrder { get; }
+            string Name { get; }
+            IDictionary<DateTime, string> Reminders { get; }
+
+            public decimal ComputeLoyaltyDiscount()
+            {
+                DateTime TwoYearsAgo = DateTime.Now.AddYears(-2);
+                if ((DateJoined < TwoYearsAgo) && (PreviousOrders.Count() > 10))
+                {
+                    return 0.10m;
+                }
+                return 0;
+            }
+            public string someMethod();
+
+        }
             public static RGBColor FromRainbow(Rainbow colorBand) =>
                 colorBand switch
                 {
