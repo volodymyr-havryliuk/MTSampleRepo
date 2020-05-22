de05319ff6f64d14c83afaedfc4e03d432a8b757
volodymyr.havryliuk <volodymyr.havryliuk@demicon.de>
2020-05-23 01:49:23 +0300

    Add struct readonly members

---
+++ rom.cs
@@ -0,0 +1,9 @@
+public struct Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Distance => Math.Sqrt(X * X + Y * Y);
+
+	public readonly override string ToString() =>
+		$"({X}, {Y}) is {Distance} from the origin";
+}
