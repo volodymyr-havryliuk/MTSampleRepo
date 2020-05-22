62965af99c5512520d9c99a877d471aab168b3b6
volodymyr.havryliuk <volodymyr.havryliuk@demicon.de>
2020-05-23 01:52:00 +0300

    Update struct rom

--- rom.cs
+++ rom.cs
@@ -2,7 +2,7 @@ public struct Point
 {
     public double X { get; set; }
     public double Y { get; set; }
-    public double Distance => Math.Sqrt(X * X + Y * Y);
+    public readonly double Distance => Math.Sqrt(X * X + Y * Y);
 
 	public readonly override string ToString() =>
 		$"({X}, {Y}) is {Distance} from the origin";
